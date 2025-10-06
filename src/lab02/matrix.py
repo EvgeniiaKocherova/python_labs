9b5ba0a7e53808e497f5e5933c8207be91855561
Evgeniia Kocherova <m2504998@edu.misis.ru>
2025-10-06 22:32:32 +0300

    добавила задание b лр 2

--- src/lab02/matrix.py
+++ src/lab02/matrix.py
@@ -1,4 +1,8 @@
-def g(func, a):
+def print_arg_ret(func, a):
+    """
+    Эта функция пишет аргументы (a) и возвращаемое значение функции (func)
+    Используется для отладки тест-кейсов
+    """
     print(f'{a} -> ', end='')
     r = func(a)
     print(r)
@@ -8,6 +12,9 @@ def g(func, a):
 print("задача 1")
 
 def transpose(mat):
+    """
+    Эта функция меняет строки и столбцы местами.
+    """
     t_mat = []
     cnt_strok = len(mat) 
     
@@ -34,18 +41,18 @@ def transpose(mat):
     
     return t_mat
 
-assert g(transpose, [[1, 2, 3]]) == [[1], [2], [3]]
-assert g(transpose, [[1], [2], [3]]) == [[1, 2, 3]]
-assert g(transpose, [[1, 2], [3, 4]]) == [[1, 3], [2, 4]]
-assert g(transpose, []) == []
+assert print_arg_ret(transpose, [[1, 2, 3]]) == [[1], [2], [3]]
+assert print_arg_ret(transpose, [[1], [2], [3]]) == [[1, 2, 3]]
+assert print_arg_ret(transpose, [[1, 2], [3, 4]]) == [[1, 3], [2, 4]]
+assert print_arg_ret(transpose, []) == []
 try:
-    g(transpose, [[1, 2], [3]])
+    print_arg_ret(transpose, [[1, 2], [3]])
     assert False
 except ValueError:
     print("ValueError")
 
 try:
-    g(transpose, [[1], [2, 3]])
+    print_arg_ret(transpose, [[1], [2, 3]])
     assert False
 except ValueError:
     print("ValueError")
@@ -55,6 +62,9 @@ print("")
 print("задача 2")
 
 def row_sums(mat):
+    """
+    Эта функция считает сумму в каждой строке матрицы.
+    """
     sum_el = []
     cnt_strok = len(mat)
     if cnt_strok == 0:
@@ -71,21 +81,24 @@ def row_sums(mat):
         sum_el[i] = s
     return sum_el
 
-assert g(row_sums, [[1, 2, 3], [4, 5, 6]]) == [6, 15]
-assert g(row_sums, [[-1, 1], [10, -10]]) == [0, 0]
-assert g(row_sums, [[0, 0], [0, 0]]) == [0, 0]
+assert print_arg_ret(row_sums, [[1, 2, 3], [4, 5, 6]]) == [6, 15]
+assert print_arg_ret(row_sums, [[-1, 1], [10, -10]]) == [0, 0]
+assert print_arg_ret(row_sums, [[0, 0], [0, 0]]) == [0, 0]
 try:
-    g(row_sums, [[1, 2], [3]])
-    assert False
+    print_arg_ret(row_sums, [[1, 2], [3]])
+    assert False, "Мы не должны сюда зайти"
 except ValueError:
     print("ValueError")
-assert g(row_sums, []) == [] 
+assert print_arg_ret(row_sums, []) == [] 
 
 #3
 print("")
 print("задача 3")
 
 def col_sums(mat):
+    """
+    Эта функция считает сумму всех столбцов матрицы.
+    """
     sum_el = []
     cnt_strok = len(mat)
     
@@ -107,14 +120,14 @@ def col_sums(mat):
     
     return sum_el
 
-assert g(col_sums, [[1, 2, 3], [4, 5, 6]]) == [5, 7, 9]
-assert g(col_sums, [[-1, 1], [10, -10]]) == [9, -9]
-assert g(col_sums, [[0, 0], [0, 0]]) == [0, 0]
+assert print_arg_ret(col_sums, [[1, 2, 3], [4, 5, 6]]) == [5, 7, 9]
+assert print_arg_ret(col_sums, [[-1, 1], [10, -10]]) == [9, -9]
+assert print_arg_ret(col_sums, [[0, 0], [0, 0]]) == [0, 0]
 try:
-    g(col_sums, [[1, 2], [3]])
-    assert False
+    print_arg_ret(col_sums, [[1, 2], [3]])
+    assert False, "Мы не должны сюда зайти"
 except ValueError:
     print("ValueError")
-assert g(col_sums, []) == []
+assert print_arg_ret(col_sums, []) == []
 
 
