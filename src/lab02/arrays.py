a2bd6f6333987a27cb6dd2291536cb3324687db0
Evgeniia Kocherova <m2504998@edu.misis.ru>
2025-10-06 22:34:02 +0300

    изменила задание 1 лр 2

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -1,4 +1,8 @@
-def g(func, a):
+def print_arg_ret(func, a):
+    """
+    Эта функция пишет аргументы (a) и возвращаемое значение функции (func)
+    Используется для отладки тест-кейсов
+    """
     print(f'{a} -> ', end='')
     r = func(a)
     print(r)
@@ -8,35 +12,44 @@ def g(func, a):
 print("задача 1")
 
 def min_max(nums):
+    """Эта функция возвращает минимум и максимум из списка.
+    Например, [3, -1, 5, 5, 0] -> (-1, 5)"""
     return (min(nums), max(nums))
 
-assert g(min_max, [3, -1, 5, 5, 0]) == (-1, 5)
-assert g(min_max, [42]) == (42, 42)
-assert g(min_max, [-5, -2, -9]) == (-9, -2)
+assert print_arg_ret(min_max, [3, -1, 5, 5, 0]) == (-1, 5)
+assert print_arg_ret(min_max, [42]) == (42, 42)
+assert print_arg_ret(min_max, [-5, -2, -9]) == (-9, -2)
 try:
-    g(min_max, [])
+    print_arg_ret(min_max, [])
     assert False
 except ValueError:
     print("ValueError")    
-assert g(min_max, [1.5, 2, 2.0, -3.1]) == (-3.1, 2)
+assert print_arg_ret(min_max, [1.5, 2, 2.0, -3.1]) == (-3.1, 2)
 
 #2
 print("")
 print("задача 2")
 
 def unique_sorted(nums):
+    """
+    Эта функция сортирует значения по возрастанию и убирает дубликаты.
+    Например, [3, -1, 5, 5, 0] → (-1, 5)
+    """
     return sorted(set(nums))
 
-assert g(unique_sorted, [3, 1, 2, 1, 3]) ==  [1, 2, 3]
-assert g(unique_sorted, []) == []             
-assert g(unique_sorted, [-1, -1, 0, 2, 2]) == [-1, 0, 2]
-assert g(unique_sorted, [1.0, 1, 2.5, 2.5, 0]) == [0, 1.0, 2.5]
+assert print_arg_ret(unique_sorted, [3, 1, 2, 1, 3]) ==  [1, 2, 3]
+assert print_arg_ret(unique_sorted, []) == []             
+assert print_arg_ret(unique_sorted, [-1, -1, 0, 2, 2]) == [-1, 0, 2]
+assert print_arg_ret(unique_sorted, [1.0, 1, 2.5, 2.5, 0]) == [0, 1.0, 2.5]
 
 #3
 print("")
 print("задача 3")
 
 def flatten0(mat, b):
+    """
+    Эта функция реализует flatten через рекурсию 
+    """
     # print(f'mat={mat}, b={b}')
     for a in mat:
         if type(a) == list:
@@ -48,15 +61,18 @@ def flatten0(mat, b):
                 raise TypeError()
             b.append(a)
 def flatten(mat):
+    """
+    Эта функция делает "расплющивание" массива: из вложенного делает плоским
+    """
     b = []
     flatten0(mat, b)
     return b
 
-assert g(flatten, [[1, 2], [3, 4]]) == [1, 2, 3, 4]
-assert g(flatten, [[1, 2], (3, 4, 5)]) == [1, 2, 3, 4, 5]
-assert g(flatten, [[1], [], [2, 3]]) == [1, 2, 3]
+assert print_arg_ret(flatten, [[1, 2], [3, 4]]) == [1, 2, 3, 4]
+assert print_arg_ret(flatten, [[1, 2], (3, 4, 5)]) == [1, 2, 3, 4, 5]
+assert print_arg_ret(flatten, [[1], [], [2, 3]]) == [1, 2, 3]
 try:
-    g(flatten, [[1, 2], "ab"])
+    print_arg_ret(flatten, [[1, 2], "ab"])
     assert False
 except TypeError:
     print("TypeError")
