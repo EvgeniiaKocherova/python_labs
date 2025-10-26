7cc136e1fd68425b81dd1ed41fb44d9fcf103f56
Evgeniia Kocherova <m2504998@edu.misis.ru>
2025-10-26 18:41:37 +0300

    добавила код и пример выполнения тест-кейсов для задания b

---
+++ src/lab05/csv_xlsx.py
@@ -0,0 +1,38 @@
+from openpyxl import Workbook
+from json_csv import *
+
+def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None:
+    """
+    Конвертирует CSV в XLSX.
+    Первая строка CSV — заголовок.
+    Колонки — автоширина по длине текста (не менее 8 символов).
+    """
+
+    rows = get_csv_as_rows(csv_path)
+
+    if not rows:
+        raise ValueError('csv-файл пустой')
+
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Sheet1"
+
+    for row in rows:
+        ws.append(row)
+
+    for col in ws.columns:
+        max_length = 0
+        column = col[0].column_letter # Get the column name
+        for cell in col:
+            try: # Necessary to avoid error on empty cells
+                if len(str(cell.value)) > max_length:
+                    max_length = len(str(cell.value))
+            except:
+                pass
+        if max_length < 8:
+            max_length = 8
+
+        adjusted_width = (max_length + 2) * 1.2
+        ws.column_dimensions[column].width = adjusted_width
+
+    wb.save(xlsx_path)
